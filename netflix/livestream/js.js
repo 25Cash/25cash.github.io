2449a5ab5ce36bd80bf0739eee189a2dbc45bd45
25cash_ <118428420+25Cash@users.noreply.github.com>
2023-07-01 22:18:20 +0200

    First commit
    New app

---
+++ netflix/livestream/js.js
@@ -0,0 +1,12 @@
+
+            <script>
+                function height() {
+                    var first = document.getElementById("frst").style.transition= ".2s linear";
+                    var first = document.getElementById("frst").style.height= "53vh";
+                }
+            </script>
+
+
+
+
+        
