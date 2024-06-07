3cadacfa5a19b84d86ffefd42c341a8dd420a0b6
25cash_ <118428420+25Cash@users.noreply.github.com>
2024-06-07 18:49:11 +0200

    Delete Bahenda_Bruno_School_Integrated_Assessments(1) directory

--- Bahenda_Bruno_School_Integrated_Assessments(1)/script.js
+++
@@ -1,8 +0,0 @@
-
-		function click(){
-			// document.getElementById('post').classlist.add('active');
-			console.log('blahh')
-		}
-
-
-			let add = document.getElementById('add').style.background = '#f00';
