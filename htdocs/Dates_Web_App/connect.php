354de5cba3a3b1e720321539b6281fefc3382d83
25cash_ <118428420+25Cash@users.noreply.github.com>
2024-06-07 18:46:41 +0200

    Delete htdocs directory

--- htdocs/Dates_Web_App/connect.php
+++
@@ -1,3 +0,0 @@
-<?php 
-$con = mysqli_connect('localhost','root','','dating_database');
-?>
--- htdocs/Dating_Web_App/Connekt.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $con = mysqli_connect("localhost","root","","dating-app");
-    if(!$con){
-        echo"System Failed To Connekt";
-    }
-?>
